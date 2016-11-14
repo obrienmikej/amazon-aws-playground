c3b64e0ee08efa4d85bd5a712754cea4587c6d10
Mike O'Brien <mike.j.obrien@gmail.com>
2016-11-14 09:43:40 -0800

    Add TABLE_NAME as variable

--- dynamodb/tables/create_table_poduct_catalog.sh
+++ dynamodb/tables/create_table_poduct_catalog.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-aws dynamodb create-table --attribute-definitions AttributeName=Id,AttributeType=N --table-name ProductCatalog  --key-schema AttributeName=Id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+TABLE_NAME=ProductCatalog
+
+aws dynamodb create-table --attribute-definitions AttributeName=Id,AttributeType=N --table-name $TABLE_NAME --key-schema AttributeName=Id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
