1a4983f5b21baf60a38d3b780fdbb28fdebabdca
Mike O'Brien <mike.j.obrien@gmail.com>
2016-11-14 09:56:23 -0800

    adding variables

--- dynamodb/create_table_forum.sh
+++ dynamodb/create_table_forum.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-aws dynamodb create-table --attribute-definitions AttributeName=Name,AttributeType=S --table-name Forum --key-schema AttributeName=Name,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+TABLE_NAME=Forum
+READ_UNITS=1
+WRITE_UNITS=1
+
+aws dynamodb create-table --attribute-definitions AttributeName=Name,AttributeType=S --table-name $TABLE_NAME --key-schema AttributeName=Name,KeyType=HASH --provisioned-throughput ReadCapacityUnits=$READ_UNITS,WriteCapacityUnits=$WRITE_UNITS
