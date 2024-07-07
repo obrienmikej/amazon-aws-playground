ae1bd1d70d58e4b9d8f746f0fdf6177635510b5f
Mike O'Brien <mike.j.obrien@gmail.com>
2024-07-07 10:58:52 -0700

    move to /hello-cdk folder

--- hello-cdk/app.py
+++ hello-cdk/app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from python.python_stack import PythonStack
+from hello_cdk.hello_cdk_stack import HelloCdkStack
 
 
 app = cdk.App()
-PythonStack(app, "PythonStack",
+HelloCdkStack(app, "HelloCdkStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
--- hello-cdk/tests/unit/test_hello_cdk_stack.py
+++ hello-cdk/tests/unit/test_hello_cdk_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from python.python_stack import PythonStack
+from hello_cdk.hello_cdk_stack import HelloCdkStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in python/python_stack.py
+# resource in hello_cdk/hello_cdk_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = PythonStack(app, "python")
+    stack = HelloCdkStack(app, "hello-cdk")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
