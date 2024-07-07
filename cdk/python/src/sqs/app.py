013bf1ad05d3345c7629a2b46a9274a9d2c62bef
Mike O'Brien <mike.j.obrien@gmail.com>
2024-07-07 14:41:46 -0700

    Update class name, no abbreviation

--- cdk/python/src/sqs/app.py
+++ cdk/python/src/sqs/app.py
@@ -6,7 +6,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class SqsQueueStack(Stack):
+class SimpleQueueServiceStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -19,6 +19,6 @@ class SqsQueueStack(Stack):
         )
 
 app = cdk.App()
-SqsQueueStack(app, "SqsQueueStack")
+SimpleQueueServiceStack(app, "SimpleQueueServiceStack")
 
 app.synth()
