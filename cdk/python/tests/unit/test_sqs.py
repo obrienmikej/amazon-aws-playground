d4dc61a5de69da697e129c634801460d66b5708b
Mike O'Brien <mike.j.obrien@gmail.com>
2024-07-09 13:53:58 -0700

    working sqs unittest

--- cdk/python/tests/unit/test_sqs.py
+++ cdk/python/tests/unit/test_sqs.py
@@ -1,14 +1,45 @@
-import aws_cdk as cdk
-import aws_cdk.assertions as assertions
+import unittest
+from aws_cdk import (
+    Stack,
+    App,
+    assertions,
+    aws_sqs as sqs,
+    Duration
+)
+from constructs import Construct
 
-from cdk.python.src.sqs.app import SimpleQueueServiceStack
+class SqsStack(Stack):
 
-def test_sqs_queue_created():
-    app = cdk.App()
-    stack = SimpleQueueServiceStack(app, "TestStack")
-    template = assertions.Template.from_stack(stack)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-    template.has_resource_properties("AWS::SQS::Queue", {
-        "QueueName": "MySimpleQueue",
-        "VisibilityTimeout": 300
-    })
+        # Create the SQS queue
+        queue = sqs.Queue(
+            self, "MyQueue",
+            queue_name="MyQueue",
+            visibility_timeout=Duration.seconds(30),
+            retention_period=Duration.days(4)
+        )
+
+class MyApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        SqsStack(self, "SqsStack")
+
+class TestSqsStack(unittest.TestCase):
+
+    def setUp(self):
+        self.app = App()
+        self.stack = SqsStack(self.app, "SqsStack")
+
+    def test_queue_created(self):
+        template = assertions.Template.from_stack(self.stack)
+
+        template.has_resource_properties("AWS::SQS::Queue", {
+            "QueueName": "MyQueue",
+            "VisibilityTimeout": 30,
+            "MessageRetentionPeriod": 345600  # 4 days in seconds
+        })
+
+if __name__ == '__main__':
+    unittest.main()
